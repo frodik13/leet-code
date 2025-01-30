5022dc4e870f75f7d5c3f91a8bdcc7b9f9541eeb
Fedor Krivoy <frodo-66@yandex.ru>
2025-01-30 20:49:20 +0400

    Move Zeroes completed

---
+++ LeetCode/LeetCode/TwoPointers/MoverZeroes.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.TwoPointers;
+
+public class MoverZeroes
+{
+    public void MoveZeroes(int[] nums)
+    {
+        var indexNonZero = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 0) continue;
+
+            (nums[i], nums[indexNonZero]) = (nums[indexNonZero], nums[i]);
+            indexNonZero++;
+        }
+    }
+}
---
+++ LeetCode/Tests/TwoPointers/MoverZeroesTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using LeetCode.TwoPointers;
+
+namespace Tests.TwoPointers;
+
+[TestFixture]
+public class MoverZeroesTest
+{
+    private readonly MoverZeroes _moveZeroes = new();
+
+    [TestCase(new int[5] {0, 1, 0, 3, 12}, new int[5] { 1, 3, 12, 0, 0 })]
+    [TestCase(new int[1] { 0 }, new int[1] { 0 })]
+    [TestCase(new int[10] { 0, 1, 0, 3, 12, 0, 6, 20, 0, 10 }, new int[10] { 1, 3, 12, 6, 20, 10, 0, 0, 0, 0 })]
+    public void Test(int[] inputArray, int[] outputArray)
+    {
+        // Arrange
+
+        // Act
+        _moveZeroes.MoveZeroes(inputArray);
+
+        // Assert
+        inputArray.Should().BeEquivalentTo(outputArray);
+    }
+}
