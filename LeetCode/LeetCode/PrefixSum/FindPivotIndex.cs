4b293627a1f9edf2fcef6852c1e393e67405798b
Fedor Krivoy <frodo-66@yandex.ru>
2025-01-31 21:42:11 +0400

    Find Pivot Index completed

---
+++ LeetCode/LeetCode/PrefixSum/FindPivotIndex.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.PrefixSum;
+
+public class FindPivotIndex
+{
+    public int PivotIndex(int[] nums)
+    {
+        var sumRight = nums.Sum();
+        var sumLeft = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            sumRight -= nums[i];
+
+            if (sumRight == sumLeft) return i;
+
+            sumLeft += nums[i];
+        }
+
+        return -1;
+    }
+}
---
+++ LeetCode/Tests/PrefixSum/FindPivotIndexTest.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LeetCode.PrefixSum;
+
+namespace Tests.PrefixSum;
+
+[TestFixture]
+public class FindPivotIndexTest
+{
+    [TestCase(new[]{ 1, 7, 3, 6, 5, 6 }, 3)]
+    [TestCase(new[]{ 1, 2, 3 }, -1)]
+    [TestCase(new[]{ 2, 1, -1 }, 0)]
+    public void Test(int[] nums, int pivotIndex)
+    {
+        // Arrange
+        var findPivotIndex = new FindPivotIndex();
+
+        // Act
+        var result = findPivotIndex.PivotIndex(nums);
+
+        // Assert
+        result.Should().Be(pivotIndex);
+    }
+}
