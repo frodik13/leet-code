c1d48a8550d244a587d6823a07a568fd01372027
Fedor Krivoy <frodo-66@yandex.ru>
2025-01-31 21:18:42 +0400

    completed Find Highest Altitude

---
+++ LeetCode/LeetCode/PrefixSum/FindHighestAltitude.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.PrefixSum;
+
+public class FindHighestAltitude
+{
+    public int LargestAltitude(int[] gain)
+    {
+        var largestAltitude = 0;
+        var temp = 0;
+        foreach (var value in gain)
+        {
+            temp += value;
+            if (temp > largestAltitude)
+            {
+                largestAltitude = temp;
+            }
+        }
+
+        return largestAltitude;
+    }
+}
---
+++ LeetCode/Tests/PrefixSum/FindHighestAltitudeTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using LeetCode.PrefixSum;
+
+namespace Tests.PrefixSum;
+
+[TestFixture]
+public class FindHighestAltitudeTest
+{
+    [TestCase(new[]{ -5, 1, 5, 0, -7 }, 1)]
+    [TestCase(new[]{ -4, -3, -2, -1, 4, 3, 2 }, 0)]
+    public void Test(int[] gain, int highestAltitude)
+    {
+        // Arrange
+        var findHighestAltitude = new FindHighestAltitude();
+
+        // Act
+        var result = findHighestAltitude.LargestAltitude(gain);
+
+        // Assert
+        result.Should().Be(highestAltitude);
+    }
+}
