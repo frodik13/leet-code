af331263ff251f26945b08cddd213966e5bb424d
Fedor Krivoy <frodo-66@yandex.ru>
2025-02-01 13:38:51 +0400

    solved FindDifferenceOfTwoArrays

---
+++ LeetCode/LeetCode/HashMap/FindDifferenceOfTwoArrays.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.HashMap;
+
+public class FindDifferenceOfTwoArrays
+{
+    public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+    {
+        var set1 = new HashSet<int>(nums1);
+        var set2 = new HashSet<int>(nums2);
+
+        set1.ExceptWith(nums2);
+        set2.ExceptWith(nums1);
+
+        return new List<IList<int>>()
+        {
+            set1.ToList(),
+            set2.ToList()
+        };
+    }
+}
---
+++ LeetCode/Tests/HashMap/FindDifferenceOfTwoArraysTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using LeetCode.HashMap;
+
+namespace Tests.HashMap;
+
+public class FindDifferenceOfTwoArraysTest
+{
+    private static readonly Dictionary<int, IList<IList<int>>> Answers = new()
+    {
+        { 1, new List<IList<int>>(){ new List<int>{ 1, 3 }, new List<int>{ 4, 6 } }},
+        { 2, new List<IList<int>>(){ new List<int>{ 3 }, new List<int>{}}},
+    };
+
+
+    [TestCase(new[] { 1, 2, 3 }, new[]{ 2, 4, 6 }, 1)]
+    [TestCase(new[] { 1, 2, 3, 3 }, new[]{ 1, 1, 2, 2 }, 2)]
+    public void Test(int[] nums1, int[] nums2, int indexAnswers)
+    {
+        // Arrange
+        var finder = new FindDifferenceOfTwoArrays();
+
+        // Act
+        var result = finder.FindDifference(nums1, nums2);
+
+        // Assert
+        result.Should().BeEquivalentTo(Answers[indexAnswers]);
+    }
+}
