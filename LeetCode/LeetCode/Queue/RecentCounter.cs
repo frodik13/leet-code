39d420774fc6474de1a4d292108e99e0abb22b86
Fedor Krivoy <frodo-66@yandex.ru>
2025-02-01 14:05:14 +0400

    solved RecentCounter

---
+++ LeetCode/LeetCode/Queue/RecentCounter.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Queue;
+
+public class RecentCounter
+{
+    private readonly Queue<int> _queue = new();
+
+    public int Ping(int t)
+    {
+        _queue.Enqueue(t);
+
+        while (_queue.Peek() < t - 3000)
+        {
+            _queue.Dequeue();
+        }
+
+        return _queue.Count;
+    }
+}
---
+++ LeetCode/Tests/Queue/RecentCounterTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using LeetCode.Queue;
+
+namespace Tests.Queue;
+
+public class RecentCounterTest
+{
+    private readonly RecentCounter _recentCounter = new();
+
+    [TestCase(1, 1)]
+    [TestCase(100, 2)]
+    [TestCase(3001, 3)]
+    [TestCase(3002, 3)]
+    public void Test(int t, int answer)
+    {
+        // Arrange
+
+        // Act
+        var count = _recentCounter.Ping(t);
+
+        // Assert
+        count.Should().Be(answer);
+    }
+}
