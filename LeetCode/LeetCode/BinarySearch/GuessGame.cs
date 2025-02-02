6dedd763fef0dc71ee3476fa8f2216a2870bb10c
Fedor Krivoy <frodo-66@yandex.ru>
2025-02-02 13:05:19 +0400

    Guess Number Higher or Lower solved

---
+++ LeetCode/LeetCode/BinarySearch/GuessGame.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Headers;
+
+namespace LeetCode.BinarySearch;
+
+public class GuessGame
+{
+    private readonly int _guessNumber;
+
+    public GuessGame(int guessNumber)
+    {
+        _guessNumber = guessNumber;
+    }
+
+    public int GuessNumber(int n)
+    {
+        var low = 1;
+        var high = n;
+
+        while (low <= high)
+        {
+            var mid = low + (high - low) / 2;
+
+            var result = guess(mid);
+
+            if (result == 0)
+                return mid;
+            else if (result == -1)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return -1;
+    }
+
+    private int guess(int num)
+    {
+        return num == _guessNumber
+            ? 0
+            : num > _guessNumber
+                ? -1
+                : 1;
+    }
+}
---
+++ LeetCode/Tests/BinarySearch/GuessGameTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using LeetCode.BinarySearch;
+
+namespace Tests.BinarySearch;
+
+[TestFixture]
+public class GuessGameTest
+{
+    [TestCase(10, 6, 6)]
+    [TestCase(1, 1, 1)]
+    [TestCase(2, 1, 1)]
+    [TestCase(100, 74, 74)]
+    [TestCase(10000000, 452876, 452876)]
+    public void Test(int n, int pick, int answer)
+    {
+        // Arrange
+        var guessGame = new GuessGame(pick);
+
+        // Act
+        var result = guessGame.GuessNumber(n);
+
+        // Assert
+        result.Should().Be(answer);
+    }
+}
