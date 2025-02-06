4ea47e0ddc79b6383b1a3b5280eca2708c492a94
Fedor Krivoy <frodo-66@yandex.ru>
2025-02-06 20:31:51 +0400

    MaxNumberOfKSumPairs solved

---
+++ LeetCode/LeetCode/TwoPointers/MaxNumberOfKSumPairs.cs
@@ -0,0 +1,75 @@
+﻿namespace LeetCode.TwoPointers;
+
+public class MaxNumberOfKSumPairs
+{
+    public int MaxOperationsX(int[] nums, int k)
+    {
+        var numsList = nums.ToList();
+        var index1 = 0;
+        var index2 = 1;
+        var count = 0;
+
+        while (index1 < numsList.Count)
+        {
+            var number1 = numsList[index1];
+            if (number1 > k)
+            {
+                numsList.RemoveAt(index1);
+                continue;
+            }
+            
+            while (index2 < numsList.Count)
+            {
+                var number2 = numsList[index2];
+                if (number2 > k)
+                {
+                    numsList.RemoveAt(index2);
+                    continue;
+                }
+
+                if (number2 + number1 == k)
+                {
+                    numsList.RemoveAt(index1);
+                    numsList.RemoveAt(index2 - 1);
+                    index1 = -1;
+                    count++;
+                    break;
+                }
+
+                index2++;
+            }
+
+            index1++;
+            index2 = index1 + 1;
+
+        }
+
+        return count;
+    }
+
+    public int MaxOperations(int[] nums, int k)
+    {
+        Array.Sort(nums);
+
+        var count = 0;
+        var left = 0;
+        var right = nums.Length - 1;
+        while (left < right)
+        {
+            var sum = nums[left] + nums[right];
+
+            if (sum > k)
+                right--;
+            else if (sum < k)
+                left++;
+            else
+            {
+                count++;
+                left++;
+                right--;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ LeetCode/Tests/TwoPointers/MaxNumberOfKSumPairsTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using LeetCode.TwoPointers;
+
+namespace Tests.TwoPointers;
+
+[TestFixture]
+public class MaxNumberOfKSumPairsTest
+{
+    [TestCase<int[], int, int>([1, 2, 3, 4], 5, 2)]
+    [TestCase<int[], int, int>([3, 1, 3, 4, 3], 6, 1)]
+    public void Test(int[] nums, int k, int expected)
+    {
+        // Arrange
+        MaxNumberOfKSumPairs solution = new();
+
+        // Act
+        var result = solution.MaxOperations(nums, k);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
