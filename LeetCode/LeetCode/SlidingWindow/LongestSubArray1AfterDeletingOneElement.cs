626970362647bd1278a2988d6206a0ddbd4761f4
Fedor Krivoy <frodo-66@yandex.ru>
2025-02-06 21:05:45 +0400

    LongestSubArray1AfterDeletingOneElement solved

---
+++ LeetCode/LeetCode/SlidingWindow/LongestSubArray1AfterDeletingOneElement.cs
@@ -0,0 +1,82 @@
+﻿namespace LeetCode.SlidingWindow;
+
+public class LongestSubArray1AfterDeletingOneElement
+{
+    public int LongestSubarrayX(int[] nums)
+    {
+        var lastZero = -1;
+
+        List<int> counts = [];
+
+        var count = 0;
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] == 0)
+            {
+                counts.Add(count);
+                lastZero = i;
+                count = 0;
+            }
+            else
+            {
+                count++;
+            }
+        }
+
+        if (lastZero == -1)
+        {
+            return nums.Length - 1;
+        }
+
+        if (nums[^1] == 1)
+        {
+            count++;
+        }
+
+        counts.Add(count);
+
+        var maxSum = counts[0] + counts[1];
+        var tempSum = maxSum;
+
+        for (var i = 1; i < counts.Count - 1; i++)
+        {
+            tempSum -= counts[i - 1];
+            tempSum += counts[i + 1];
+            if (tempSum > maxSum)
+            {
+                maxSum = tempSum;
+            }
+        }
+        
+
+        return maxSum;
+    }
+
+    public int LongestSubarray(int[] nums)
+    {
+        var current = 0;
+        var previous = 0;
+        var result = 0;
+
+        //if (nums.All(x => x == 1)) return nums.Length - 1;
+        //if (nums.All(x => x == 0)) return 0;
+
+        foreach (var num in nums)
+        {
+            if (num == 1)
+            {
+                current++;
+            }
+            else
+            {
+                result = Math.Max(result, current + previous);
+                previous = current;
+                current = 0;
+            }
+        }
+
+        result = Math.Max(result, current + previous);
+
+        return result == nums.Length ? nums.Length - 1 : result;
+    }
+}
---
+++ LeetCode/Tests/SlidingWindow/LongestSubArray1AfterDeletingOneElementTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using LeetCode.SlidingWindow;
+
+namespace Tests.SlidingWindow;
+
+[TestFixture]
+public class LongestSubArray1AfterDeletingOneElementTest
+{
+    [TestCase<int[], int>([1, 1, 0, 1], 3)]
+    [TestCase<int[], int>([0, 1, 1, 1, 0, 1, 1, 0, 1], 5)]
+    [TestCase<int[], int>([1, 1, 1], 2)]
+    [TestCase<int[], int>([1,1,1,0,1,1,0,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1], 8)]
+    public void Test(int[] nums, int expected)
+    {
+        // Arrange
+        LongestSubArray1AfterDeletingOneElement solution = new();
+
+        // Act
+        var result = solution.LongestSubarray(nums);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
