5838f6d50f16ec727600f05aea6a90433fd9ed1c
Fedor Krivoy <frodo-66@yandex.ru>
2025-02-07 21:01:54 +0400

    DecodeStringTask solved

--- LeetCode/LeetCode/Stack/DecodeStringTask.cs
+++ LeetCode/LeetCode/Stack/DecodeStringTask.cs
@@ -1,14 +1,38 @@
+using System.Text;
+
 namespace LeetCode.Stack;
 
 public class DecodeStringTask
 {
     public string DecodeString(string s)
     {
-        var stack = new Stack<char>();
-        for (var i = 0; i < s.Length; i++)
+        var stackWords = new Stack<string>();
+        var stackNumbers = new Stack<int>();
+        
+        var currentString = string.Empty;
+        var currentNumberStr = string.Empty;
+
+        foreach (var c in s)
         {
-            
+            if (char.IsDigit(c)) currentNumberStr += c;
+
+            if (char.IsLetter(c)) currentString += c;
+
+            switch (c)
+            {
+                case '[':
+                    stackWords.Push(currentString);
+                    stackNumbers.Push(int.Parse(currentNumberStr));
+                    currentNumberStr = string.Empty;
+                    currentString = string.Empty;
+                    continue;
+                case ']':
+                    currentString = stackWords.Pop() +
+                                    string.Join("", Enumerable.Repeat(currentString, stackNumbers.Pop()));
+                    continue;
+            }
         }
-        throw new NotImplementedException();
+
+        return currentString;
     }
 }
