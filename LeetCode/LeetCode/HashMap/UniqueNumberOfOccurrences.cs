0fffc9b6213669c636b0702112b606902a29c9d2
Fedor Krivoy <frodo-66@yandex.ru>
2025-02-12 20:47:40 +0400

    UniqueNumberOfOccurrences solved

---
+++ LeetCode/LeetCode/HashMap/UniqueNumberOfOccurrences.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.HashMap;
+
+public class UniqueNumberOfOccurrences
+{
+    public bool UniqueOccurrences(int[] arr)
+    {
+        Dictionary<int, int> map = [];
+
+        foreach (var i in arr)
+        {
+            map.TryAdd(i, 0);
+            map[i]++;
+        }
+
+        HashSet<int> hashSet = [..map.Values];
+        return hashSet.Count == map.Count;
+    }
+}
---
+++ LeetCode/Tests/HashMap/UniqueNumberOfOccurrencesTest.cs
@@ -0,0 +1,20 @@
+﻿namespace Tests.HashMap;
+
+[TestFixture]
+public class UniqueNumberOfOccurrencesTest
+{
+    [TestCase<int[], bool>([1, 2, 2, 1, 1, 3], true)]
+    [TestCase<int[], bool>([1, 2], false)]
+    [TestCase<int[], bool>([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0], true)]
+    public void Test(int[] arr, bool expected)
+    {
+        // Arrange
+        UniqueNumberOfOccurrences solution = new();
+
+        // Act
+        var result = solution.UniqueOccurrences(arr);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
