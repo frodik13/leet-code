30fb18e35cd5d5f471bd9e52a799745519b73a8a
Fedor Krivoy <frodo-66@yandex.ru>
2025-02-20 19:38:11 +0400

    FindPeakElement solved

---
+++ LeetCode/LeetCode/BinarySearch/FindPeakElementSolution.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCode.BinarySearch;
+
+public class FindPeakElementSolution
+{
+    public int FindPeakElement(int[] nums)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left < right)
+        {
+            var middle = (left + right) / 2;
+
+            if (nums[middle] > nums[middle + 1])
+                right = middle;
+            else
+                left = middle + 1;
+        }
+
+        return left;
+    }
+
+    public int FindPeakElementY(int[] nums)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left < right)
+        {
+            var middle = left + (right - left) / 2;
+            if (middle > 0 && nums[middle - 1] > nums[middle])
+                right = middle - 1;
+            else if (middle < nums.Length - 1 && nums[middle + 1] > nums[middle])
+                left = middle + 1;
+            else
+                return middle;
+        }
+
+        return right;
+    }
+
+    public int FindPeakElementX(int[] nums)
+    {
+        var findPeakIndex = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var left = i == 0 || nums[i - 1] < nums[i];
+            var right = i == nums.Length - 1 || nums[i] > nums[i + 1];
+            if (!left || !right) continue;
+            
+            findPeakIndex = i;
+            break;
+        }
+
+        return findPeakIndex;
+    }
+}
---
+++ LeetCode/Tests/BinarySearch/FindPeakElementTest.cs
@@ -0,0 +1,20 @@
+﻿namespace Tests.BinarySearch;
+
+[TestFixture]
+public class FindPeakElementTest
+{
+    [TestCase<int[],int>([1, 2, 3, 1], 2)]
+    [TestCase<int[],int>([1, 2, 1, 3, 5, 6, 4], 5)]
+    [TestCase<int[],int>([1, 2], 1)]
+    public void Test(int[] nums, int expected)
+    {
+        // Arrange
+        var solution = new FindPeakElementSolution();
+
+        // Act
+        var result = solution.FindPeakElement(nums);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
