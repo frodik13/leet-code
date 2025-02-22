cf9ff8f18720289ffe76ff907b9d2a40d716c1c5
Fedor Krivoy <frodo-66@yandex.ru>
2025-02-22 13:36:24 +0400

    EditDistance solved

---
+++ LeetCode/LeetCode/DynamicProgramming/EditDistance.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.DynamicProgramming;
+
+public class EditDistance
+{
+    public int MinDistance(string word1, string word2)
+    {
+        if (word1.Length == 0 && word2.Length == 0) return 0;
+        if (word1.Length == 0) return word2.Length;
+        if (word2.Length == 0) return word1.Length;
+
+        var m = word1.Length;
+        var n = word2.Length;
+
+        var table = new int[m + 1, n + 1];
+        for (var i = 0; i <= m; i++)
+            table[i, 0] = i;
+
+        for (var i = 0; i <= n; i++)
+            table[0, i] = i;
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                if (word1[i] == word2[j])
+                    table[i + 1, j+ 1] = Math.Min(table[i,j], Math.Min(table[i, j+ 1] + 1, table[i + 1, j] + 1));
+                else
+                    table[i +1, j+ 1] = Math.Min(table[i, j], Math.Min(table[i, j + 1], table[i +1, j])) + 1;
+            }
+        }
+
+        return table[m,n];
+    }
+}
---
+++ LeetCode/Tests/DynamicProgramming/EditDistanceTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Tests.DynamicProgramming;
+
+[TestFixture]
+public class EditDistanceTest
+{
+    [TestCase("horse", "ros", 3)]
+    [TestCase("intention", "execution", 5)]
+    public void Test(string word1, string word2, int expected)
+    {
+        // Arrange
+        var solution = new EditDistance();
+
+        // Act
+        var result = solution.MinDistance(word1, word2);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
