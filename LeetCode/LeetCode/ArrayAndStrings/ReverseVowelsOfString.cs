fcd9e45dd1c6789d412e3e7ea372404f2df45ac1
Fedor Krivoy <frodo-66@yandex.ru>
2025-02-26 21:52:32 +0400

    ReverseVowelsOfString solved

---
+++ LeetCode/LeetCode/ArrayAndStrings/ReverseVowelsOfString.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace LeetCode.ArrayAndStrings;
+
+public class ReverseVowelsOfString
+{
+    private readonly HashSet<char> _vowels = ['A', 'a', 'E', 'e', 'U', 'u', 'I', 'i', 'O', 'o'];
+    public string ReverseVowels(string s)
+    {
+        var left = 0;
+        var right = s.Length - 1;
+
+        var result = s.ToCharArray();
+
+        var stringBuilder = new StringBuilder(s);
+
+        while (left <= right)
+        {
+            while (!_vowels.Contains(s[left]))
+            {
+                result[left] = s[left];
+                left++;
+                if (left >= right)
+                    break;
+            }
+            
+            while (!_vowels.Contains(s[right]))
+            {
+                result[right] = s[right];
+                right--;
+                if (left >= right)
+                    break;
+            }
+
+            if (left >= right)
+                break;
+
+            result[right] = s[left];
+            result[left] = s[right];
+            
+
+            left++;
+            right--;
+        }
+
+        return new string(result);
+    }
+}
---
+++ LeetCode/Tests/ArrayAndStrings/ReverseVowelsOfStringTest.cs
@@ -0,0 +1,20 @@
+﻿namespace Tests.ArrayAndStrings;
+
+[TestFixture]
+public class ReverseVowelsOfStringTest
+{
+    [TestCase("IceCreAm", "AceCreIm")]
+    [TestCase("leetcode", "leotcede")]
+    [TestCase("a.", "a.")]
+    public void Test(string s, string expected)
+    {
+        // Arrange
+        var solution = new ReverseVowelsOfString();
+
+        // Act
+        var result = solution.ReverseVowels(s);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
