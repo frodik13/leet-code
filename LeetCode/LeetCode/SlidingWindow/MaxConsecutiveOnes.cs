f4d86df8f7e3fa0ab0409c7fd0043b290c97fa18
Fedor Krivoy <frodo-66@yandex.ru>
2025-03-01 10:38:17 +0400

    MaxConsecutiveOnes solved

---
+++ LeetCode/LeetCode/SlidingWindow/MaxConsecutiveOnes.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.SlidingWindow;
+
+public class MaxConsecutiveOnes
+{
+    public int LongestOnes(int[] nums, int k)
+    {
+        var i = 0;
+        int j;
+        for (j = 0; j < nums.Length; ++j)
+        {
+            if (nums[j] == 0) k--;
+            if (k < 0 && nums[i++] == 0) k++;
+        }
+
+        return j - i;
+    }
+}
---
+++ LeetCode/Tests/SlidingWindow/MaxConsecutiveOnesTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Tests.SlidingWindow;
+
+[TestFixture]
+public class MaxConsecutiveOnesTest
+{
+    [TestCase<int[], int, int>([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2, 6)]
+    [TestCase<int[], int, int>([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3, 10)]
+    public void Test(int[] nums, int k, int expected)
+    {
+        // Arrange
+        var solution = new MaxConsecutiveOnes();
+
+        // Act
+        var result = solution.LongestOnes(nums, k);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
