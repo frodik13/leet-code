99556d99e54e65371d674871a32d246a9adbcb38
Fedor Krivoy <frodo-66@yandex.ru>
2025-03-03 12:09:59 +0400

    MaximumTwinSum solved

---
+++ LeetCode/LeetCode/LinkedList/MaximumTwinSum.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCode.LinkedList;
+
+public class MaximumTwinSum
+{
+    public int PairSum(ListNode head)
+    {
+        if (head.next.next == null)
+        {
+            return head.val + head.next.val;
+        }
+        var middleNode = GetMiddleNode(head);
+        var reverseNode = ReverseListNode(middleNode);
+        var maxSum = 0;
+        while (reverseNode != null)
+        {
+            if (maxSum < head.val + reverseNode.val)
+            {
+                maxSum = head.val + reverseNode.val;
+            }
+
+            reverseNode = reverseNode.next;
+            head = head.next;
+        }
+
+        return maxSum;
+    }
+
+    private ListNode GetMiddleNode(ListNode head)
+    {
+        var fast = head;
+        var slow = head;
+
+        while (fast is { next: not null })
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
+    private ListNode ReverseListNode(ListNode head)
+    {
+        ListNode reverseListNode = null;
+        var currentListNode = head;
+        while (currentListNode != null)
+        {
+            var next = currentListNode.next;
+            currentListNode.next = reverseListNode;
+            reverseListNode = currentListNode;
+            currentListNode = next;
+        }
+        return reverseListNode;
+    }
+}
---
+++ LeetCode/Tests/LinkedList/MaximumTwinSumTest.cs
@@ -0,0 +1,28 @@
+﻿namespace Tests.LinkedList;
+
+[TestFixture]
+public class MaximumTwinSumTest
+{
+    private readonly Dictionary<int, ListNode> _testCases = new()
+    {
+        { 1, new ListNode(5, new ListNode(4, new ListNode(2, new ListNode(1)))) },
+        { 2, new ListNode(4, new ListNode(2, new ListNode(2, new ListNode(3)))) },
+        { 3, new ListNode(1, new ListNode(100000)) }
+    };
+
+    [TestCase(1, 6)]
+    [TestCase(2, 7)]
+    [TestCase(3, 100001)]
+    public void Test(int testCase, int expected)
+    {
+        // Arrange
+        var solution = new MaximumTwinSum();
+        var head = _testCases[testCase];
+
+        // Act
+        var result = solution.PairSum(head);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
