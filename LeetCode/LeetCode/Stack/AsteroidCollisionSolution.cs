79110ce22d61dbc0f7f9c148857729f2e8edc8e1
Fedor Krivoy <frodo-66@yandex.ru>
2025-03-03 07:49:34 +0400

    AsteroidCollision solved

---
+++ LeetCode/LeetCode/Stack/AsteroidCollisionSolution.cs
@@ -0,0 +1,67 @@
+﻿namespace LeetCode.Stack;
+
+public class AsteroidCollisionSolution
+{
+    public int[] AsteroidCollision(int[] asteroids)
+    {
+        var result = new List<int>();
+
+        for (var i = 0; i < asteroids.Length; i++)
+        {
+            if (result.Count > 0 &&
+                result[^1] > 0 &&
+                asteroids[i] < 0)
+            {
+                if (result[^1] > -asteroids[i]) continue;
+
+                if (result[^1] < -asteroids[i]) i--;
+                result.RemoveAt(result.Count - 1);
+            }
+            else
+            {
+                result.Add(asteroids[i]);
+            }
+        }
+
+        return [.. result];
+    }
+
+    public int[] AsteroidCollisionX(int[] asteroids)
+    {
+        var stack = new Stack<int>();
+        foreach (var asteroid in asteroids)
+        {
+            if (stack.Count == 0)
+            {
+                stack.Push(asteroid);
+                continue;
+            }
+
+            if (asteroid > 0)
+            {
+                stack.Push(asteroid);
+                continue;
+            }
+
+            while (stack.Count > 0 && stack.Peek() > 0 && stack.Peek() < Math.Abs(asteroid))
+                stack.Pop();
+
+            if (stack.Count == 0)
+            {
+                stack.Push(asteroid);
+                continue;
+            }
+
+            if (stack.Peek() == Math.Abs(asteroid))
+            {
+                stack.Pop();
+                continue;
+            }
+
+            if (stack.Peek() < 0)
+                stack.Push(asteroid);
+        }
+
+        return stack.Reverse().ToArray();
+    }
+}
---
+++ LeetCode/Tests/Stack/AsteroidCollisionTest.cs
@@ -0,0 +1,20 @@
+﻿namespace Tests.Stack;
+
+[TestFixture]
+public class AsteroidCollisionTest
+{
+    [TestCase<int[], int[]>([5, 10, -5], [5, 10])]
+    [TestCase<int[], int[]>([8, -8], [])]
+    [TestCase<int[], int[]>([10, 2, -5], [10])]
+    public void Test(int[] asteroids, int[] expected)
+    {
+        // Arrange
+        var solution = new AsteroidCollisionSolution();
+
+        // Act
+        var result = solution.AsteroidCollision(asteroids);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+}
