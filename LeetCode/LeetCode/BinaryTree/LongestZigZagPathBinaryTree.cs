bc7413e95c4f3fa7d7480c4222b38127c3123f03
Fedor <103507712+frodik13@users.noreply.github.com>
2025-03-11 11:53:35 +0400

    LongestZigZagPathBinaryTree solved

---
+++ LeetCode/LeetCode/BinaryTree/LongestZigZagPathBinaryTree.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.BinaryTree;
+
+public class LongestZigZagPathBinaryTree
+{
+    private int _max;
+
+    public int LongestZigZag(TreeNode root)
+    {
+        DFS(root, 0, 0);
+        return _max;
+    }
+
+    private void DFS(TreeNode node, int left, int right)
+    {
+        _max = Math.Max(_max, left);
+        _max = Math.Max(_max, right);
+
+        if (node.left != null)
+        {
+            DFS(node.left, right + 1, 0);
+        }
+
+        if (node.right != null)
+        {
+            DFS(node.right, 0, left + 1);
+        }
+    }
+}
---
+++ LeetCode/Tests/BinaryTree/LongestZigZagPathBinaryTreeTest.cs
@@ -0,0 +1,52 @@
+﻿namespace Tests.BinaryTree;
+
+[TestFixture]
+public class LongestZigZagPathBinaryTreeTest
+{
+    private readonly Dictionary<int, TreeNode> _treeCases = new()
+    {
+        {
+            1, new TreeNode(
+                val: 1,
+                right: new TreeNode(
+                    val: 1,
+                    left: new TreeNode(1),
+                    right: new TreeNode(
+                        val: 1,
+                        left: new TreeNode(
+                            val: 1,
+                            right: new TreeNode(1, right: new TreeNode(1))),
+                        right: new TreeNode(1))))
+        },
+        {
+            2, new TreeNode(
+                val: 1,
+                left: new TreeNode(
+                    val: 1,
+                    right: new TreeNode(
+                        val: 1,
+                        left: new TreeNode(1, right: new TreeNode(1)),
+                        right: new TreeNode(1))),
+                right: new TreeNode(1))
+        },
+        {
+            3, new TreeNode(1)
+        }
+    };
+
+    [TestCase(1, 3)]
+    [TestCase(2, 4)]
+    [TestCase(3, 0)]
+    public void Test(int treeCase, int expected)
+    {
+        // Arrange
+        var solution = new LongestZigZagPathBinaryTree();
+        var root = _treeCases[treeCase];
+
+        // Act
+        var result = solution.LongestZigZag(root);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
